d20b03879176433605c176032a76233c4881dca6
Henrysyh2000 <15921942097@139.com>
2019-09-20 19:29:09 +0800

    Question 3

--- Question4_pascal.py
+++ Question4_pascal.py
@@ -4,7 +4,17 @@ def pascal(n):
 
     :return: List[List[Int]] -- a list of sublists, which contains pascal values.
     """
-    pass
+    if n == 1:
+        return [[1]]
+    def new(lst1):
+        if len(lst1) == 1:
+            return [1]
+        mid = lst1[0] + lst1[1]
+        return new(lst1[1 : ]) + [mid]
+    new = new(pascal(n - 1)[-1]) + [1]
+    return pascal(n - 1) + [new]
+    
+        
 
 def main():
     print(pascal(4))    # [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]]
